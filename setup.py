e2b0d1c36709fc29193deb3babe1828511eae469
AyettGh <ghribiayett@gmail.com>
2025-10-30 23:11:15 +0100

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 #for installing local pacckage in virtual env
+from setuptools import find_packages,setup
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='AyettGh',
+    author_email='ghribiayett@gmail.com',
+    install_requires=['openai','langchain','streamlit','python-dotenv','PyPDF2'],
+    packages=find_packages()#finding local packages from local directory
+)
